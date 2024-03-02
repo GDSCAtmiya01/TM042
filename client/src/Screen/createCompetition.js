efcdf3947c7d310df7e556473f87163184ca3a8e
madhav <109799001+madhav037@users.noreply.github.com>
2024-03-02 18:42:13 +0530

    sssssssssss

--- client/src/Screen/createCompetition.js
+++ client/src/Screen/createCompetition.js
@@ -2,77 +2,17 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import "../css/createCompetition.css"
-import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
-} from "firebase/storage";
-import { app } from "../firebase";
 
 export default function CreateCompetition() {
 
   const [event, setEvent] = useState({});
   const params = useParams();
   const navigate = useNavigate();
-  const [file, setFile] = useState([]);
-
-
   const [credentials, setCredentials] = useState({
     title: "", description: "", universityName: "", eventName: "", startingDate: "", endingDate: "", location: "", totalParticipants: 0,
     image: "", registrationOpen: "", registrationStartDate: "", registrationEndDate: "", allowTeams: false, teams: [], limitOfMembers: "", createdBy: ""
   });
 
-  const handleSingleImageSubmit = (e) => {
-    if (file && file.size <= 2097152) {
-        storeSingleImage(file)
-        .then((url) => {
-          setCredentials({
-            ...credentials,
-            image: url,
-          });
-
-          console.log(credentials)
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else {
-      alert("Please select image less than 2MB");
-    }
-  };
-
-  const storeSingleImage = async (file) => {
-    return new Promise((resolve, reject) => {
-      const storage = getStorage(app);
-      const fileName = new Date().getTime() + file.name;
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("progress : " + progress);
-        },
-        (error) => {
-          reject(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL);
-          });
-        }
-      );
-    });
-  };
-
-  const handleRemoveSingleImage = () => {
-    setCredentials({
-      ...credentials,
-      imageUrl: null,
-    });
-  };
   useEffect(() => {
     const fetchevent = async () => {
       try {
@@ -261,40 +201,10 @@ export default function CreateCompetition() {
               </div>
             </div>
             <div style={{ marginBottom: "10px" }}>
-            <div className="flex gap-4">
-                  <input
-                    className="p-3 border border-gray-300 rounded-lg w-full"
-                    onChange={(e) => {
-                      setFile(e.target.files[0]); // Only store the first file
-                    }}
-                    type="file"
-                    id="image"
-                    accept="image/*"
-                  />
-                  <button
-                    type="button"
-                    onClick={handleSingleImageSubmit}
-                    className="p-3 text-green-700 border border-green-700 rounded-lg uppercase hover:shadow-lg disabled:opacity-80 disabled:cursor-not-allowed"
-                  >
-                    Upload
-                  </button>
-                </div>
-                {credentials.image && (
-                  <div className="flex justify-between p-3 border items-center">
-                    <img
-                      src={credentials.image}
-                      alt={`Listing image`}
-                      className="w-20 h-20 object-contain rounded-lg"
-                    />
-                    <button
-                      className="p-3 text-red-700 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-                      type="button"
-                      onClick={handleRemoveSingleImage}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                )}
+              <div class="lablec">Image</div>
+              <div className='fieldc' style={{ height: "40px" }}>
+                <input type='text' name='image' value={credentials.image} onChange={onchange} placeholder='Image url here' />
+              </div>
             </div>
             <div style={{ display: "flex", justifyContent: "center", marginBottom: "5px" }}>
               <button className='btn' value={'addMore'} style={{ marginRight: "10px" }} onClick={handleSubmit}>Add More</button>
